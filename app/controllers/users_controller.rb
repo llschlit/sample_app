819ba36601bd6da6b854d6259cbb6f03fa1054ee
Laura Schlittler <llschlit@gmail.com>
2015-02-04 16:41:41 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])  # find which user was called via the web address
+                                    # blah.com/users/<1>
   end
+
+  def new
+    @user = User.new                # create a new user object to pass to the form_for
+                                    # helper which builds the form based on
+                                    # a user's attributes
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user)   # redirect_to is a rails method that redirects the browser
+                                    # to the passed url, user_url(@user)
+                                    # generates the html path to the user's show action
+      flash[:success] = "Welcome to the Sample App!"
+    else
+      render 'new'                  # reloads the new page
+    end
+  end
+
+  private
+#------------------------------------------------------------------------------ 
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
